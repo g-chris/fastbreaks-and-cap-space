6cf61f853f4ec829902345a9175cc88388f9e45e
g-chris <gcv.github@gmail.com>
2025-04-23 16:39:46 -0400

    season sim with dummy scores

--- game_sim_engine.py
+++ game_sim_engine.py
@@ -1,2 +1,18 @@
 #Take a View for each team and generate scores
 #Publish game stats for each player to a fact table
+
+
+def game_sim(home_team_id, away_team_id, conn):
+    cursor = conn.cursor()
+
+    def get_team_score(team_id):
+        view_name = f"team_{team_id}_roster"
+        cursor.execute(f"SELECT overall_score FROM {view_name}")
+        player_ratings = cursor.fetchall()
+        score = sum([rating[0] for rating in player_ratings])
+        return int(score)  # round to int for score
+
+    home_score = get_team_score(home_team_id)
+    away_score = get_team_score(away_team_id)
+
+    return home_score, away_score
--- season_manager.py
+++ season_manager.py
@@ -1,4 +1,5 @@
 import sqlite3
+import game_sim_engine
 
 def create_team_roster_view(conn, team_id):
     cursor = conn.cursor()
@@ -15,10 +16,7 @@ def create_team_roster_view(conn, team_id):
             p.first_name,
             p.last_name,
             p.position,
-            p.overall_rating,
-            p.height,
-            p.weight,
-            p.birth_year
+            p.overall_score
         FROM dim_player_transactions t
         JOIN dim_players p ON t.player_id = p.player_id
         WHERE t.team_id = {team_id} AND t.etl_current_flag = 1
@@ -37,7 +35,9 @@ def simulate_game(game_id, game_day, home_team_id, away_team_id, conn):
     home_team_name = f"{home_team[0]} {home_team[1]}"
     away_team_name = f"{away_team[0]} {away_team[1]}"
 
-    print(f"Day {game_day} | Game {game_id}: {away_team_name} @ {home_team_name}")
+    home_score, away_score = game_sim_engine.game_sim(home_team_id, away_team_id, conn)
+
+    print(f"Day {game_day} | Game {game_id}: {away_team_name} {away_score} @ {home_team_name} {home_score}")
 
 
 def run_season_schedule(db_name, current_season):
